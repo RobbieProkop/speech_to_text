5a08833d8d7c4a0739cb0299103f13c42a05006d
Robbie Prokop <robbieprokop@gmail.com>
2022-12-06 16:49:57 +0400

    put recording into funciton

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-  print("Say something")
-  audio = r.listen(source)
-  voice_data = r.recognize_google(audio)
-  print(voice_data)
+def record_audio():
+  with sr.Microphone() as source:
+    audio = r.listen(source)
+    voice_data = ""
+    try:
+      voice_data = r.recognize_google(audio)
+    except sr.UnknownValueError:
+      print('Sorry, I did not understand that')
+    except sr.RequestError:
+      print("Apologies, my speech service is down")
+  
+    return voice_data
+
+print("How can I help you?")
+voice_data = record_audio()
+
+    
+  
   
   
