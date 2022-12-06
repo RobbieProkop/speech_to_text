16ade857443d8b5f7a85310988e12dafee189c98
Robbie Prokop <robbieprokop@gmail.com>
2022-12-06 17:21:48 +0400

    call yulia_speak

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ r = sr.Recognizer()
 def record_audio(ask = False):
   with sr.Microphone() as source:
     if ask:
-      print(ask)
+      yulia_speak(ask)
     audio = r.listen(source)
     voice_data = ""
     try:
       voice_data = r.recognize_google(audio)
     except sr.UnknownValueError:
-      print('Sorry, I did not understand that')
+      yulia_speak('Sorry, I did not understand that')
     except sr.RequestError:
-      print("Apologies, my speech service is down")
+      yulia_speak("Apologies, my speech service is down")
   
     return voice_data
 
@@ -39,33 +39,36 @@ def yulia_speak(audio_string):
 
 def respond(voice_data):
   if "what is your name" in voice_data:
-    print("My name is Yulia")
+    yulia_speak("My name is Yulia")
   if 'what time is it' in voice_data:
-    print(ctime())
-  if 'search' in voice_data:
+    yulia_speak(ctime())
+  if 'what date is it' in voice_data:
+    yulia_speak(ctime())
+  if 'yulia search' in voice_data:
     search = record_audio("what do you want to search for?")
     # searches brave with the search term
     url = 'https://search.brave.com/search?q=' + search
     # opens a new web browser with the search
     webbrowser.get().open(url)
-    print("here is what found for " + search)
+    yulia_speak("here is what found for " + search)
     
   # location with google maps
-  if 'find location' in voice_data:
+  if ' yulia find location' in voice_data:
     location = record_audio("what is the location?")
     # searches brave with the search term
     url = 'https://google.nl/maps/place/' + location + "/&amp;"
     # opens a new web browser with the search
     webbrowser.get().open(url)
-    print('Here is the location of ' + location)
+    yulia_speak('Here is the location of ' + location)
     
   # exit / close the program
   if "exit" in voice_data:
+    yulia_speak('Goodbye')
     exit()
 # continuouse listening
 time.sleep(1)
 
-print("How can I help you?")
+yulia_speak("How can I help you?")
 while 1:
   voice_data = record_audio()
   respond(voice_data)
