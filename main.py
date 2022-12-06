cae8059837c63d61a5ba00209a2c3faeec9b5faf
Robbie Prokop <robbieprokop@gmail.com>
2022-12-06 17:01:21 +0400

    add search term and open browser

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import speech_recognition as sr
+import webbrowser
+
+# tell the time
+from time import ctime
 
 r = sr.Recognizer()
 
-def record_audio():
+def record_audio(ask = False):
   with sr.Microphone() as source:
+    if ask:
+      print(ask)
     audio = r.listen(source)
     voice_data = ""
     try:
@@ -15,10 +21,24 @@ def record_audio():
   
     return voice_data
 
+def respond(voice_data):
+  if "what is your name" in voice_data:
+    print("My name is Yulia")
+  if 'what time is it' in voice_data:
+    print(ctime())
+  if 'search' in voice_data:
+    search = record_audio("what do you want to search for?")
+    # searches brave with the search term
+    url = 'https://search.brave.com/search?q=' + search
+    # opens a new web browser with the search
+    webbrowser.get().open(url)
+    print("here is what found for " + search)
+
 print("How can I help you?")
 voice_data = record_audio()
+respond(voice_data)
 
-    
+
   
   
   
